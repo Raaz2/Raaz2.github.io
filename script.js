00cbaf396e48ba32604acfd8f6accda58a9aed8d
Raj Mishra <rajeevranjanmishra4339@gmail.com>
2023-04-25 11:53:52 +0530

    about

--- script.js
+++ script.js
@@ -18,3 +18,39 @@ hamburger.addEventListener('click', () => {
         }
     });
 });
+
+//for typing change in about
+const textArray = [  "Java Backend Developer.", "Problem Solver." ,"JavaScript Expert.",  "Effective Communicator.",  "Team Player."];
+const typingDelay = 100;
+const erasingDelay = 100;
+const newTextDelay = 2000;
+let textArrayIndex = 0;
+let charIndex = 0;
+const typedTextSpan = document.getElementById("typed-text");
+const cursorSpan = document.querySelector(".typed-cursor");
+
+function type() {
+    if (charIndex < textArray[textArrayIndex].length) {
+        typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+        charIndex++;
+        setTimeout(type, typingDelay);
+    } else {
+        setTimeout(erase, newTextDelay);
+    }
+}
+
+function erase() {
+    if (charIndex > 0) {
+        typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex-1);
+        charIndex--;
+        setTimeout(erase, erasingDelay);
+    } else {
+        textArrayIndex++;
+        if(textArrayIndex>=textArray.length) textArrayIndex=0;
+        setTimeout(type, typingDelay + 100);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    if(textArray.length) setTimeout(type, newTextDelay + 50);
+});
