3fb745b2b5bd0b03e49db702f25a0f4964781d83
Raaz2 <rajeevranjanmishra4339@gmail.com>
2024-05-14 15:37:57 +0530

    hamburger fixed

--- script.js
+++ script.js
@@ -3,20 +3,25 @@ const navLinks = document.querySelector('nav ul');
 const links = document.querySelectorAll('nav ul li');
 
 hamburger.addEventListener('click', () => {
-    // Toggle hamburger menu
-    hamburger.classList.toggle('active');
+  // Toggle hamburger menu
+  hamburger.classList.toggle('active');
 
-    // Toggle nav links
-    navLinks.classList.toggle('active');
+  // Toggle nav links
+  navLinks.classList.toggle('active');
 
-    // Animate nav links
-    links.forEach((link, index) => {
-        if (link.style.animation) {
-            link.style.animation = '';
-        } else {
-            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
-        }
+  // Animate nav links
+  links.forEach((link, index) => {
+    link.addEventListener('click', function () {
+      navLinks.classList.remove('active');
+      hamburger.classList.remove('active');
     });
+
+    if (link.style.animation) {
+      link.style.animation = '';
+    } else {
+      link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+    }
+  });
 });
 
 //for typing change in about
@@ -30,29 +35,29 @@ const typedTextSpan = document.getElementById("typed-text");
 const cursorSpan = document.querySelector(".typed-cursor");
 
 function type() {
-    if (charIndex < textArray[textArrayIndex].length) {
-        typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
-        charIndex++;
-        setTimeout(type, typingDelay);
-    } else {
-        setTimeout(erase, newTextDelay);
-    }
+  if (charIndex < textArray[textArrayIndex].length) {
+    typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, typingDelay);
+  } else {
+    setTimeout(erase, newTextDelay);
+  }
 }
 
 function erase() {
-    if (charIndex > 0) {
-        typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex - 1);
-        charIndex--;
-        setTimeout(erase, erasingDelay);
-    } else {
-        textArrayIndex++;
-        if (textArrayIndex >= textArray.length) textArrayIndex = 0;
-        setTimeout(type, typingDelay + 100);
-    }
+  if (charIndex > 0) {
+    typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex - 1);
+    charIndex--;
+    setTimeout(erase, erasingDelay);
+  } else {
+    textArrayIndex++;
+    if (textArrayIndex >= textArray.length) textArrayIndex = 0;
+    setTimeout(type, typingDelay + 100);
+  }
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-    if (textArray.length) setTimeout(type, newTextDelay + 50);
+  if (textArray.length) setTimeout(type, newTextDelay + 50);
 });
 
 
